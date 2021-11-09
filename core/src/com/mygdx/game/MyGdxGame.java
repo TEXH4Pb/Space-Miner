d095be7272d947e3a8b78ebbd0f2be45a89dd485
TEXH4Pb <9digger9@gmail.com>
2021-11-10 02:48:58 +0300

    Create Ship with controls
    also screen is scaled now

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,52 +2,88 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.codeandweb.physicseditor.PhysicsShapeCache;
 
 public class MyGdxGame extends ApplicationAdapter {
+	static final float STEP_TIME = 1f / 60f;
+	static final int VELOCITY_ITERATIONS = 6;
+	static final int POSITION_ITERATIONS = 2;
+	static final float SCALE = 0.02f;
+
 	SpriteBatch batch;
-	Texture space;
+	Sprite space;
 	private OrthographicCamera camera;
 	int screenWidth, screenHeight;
-	PhysicsShapeCache shapes;
+	World world;
+	Ship player;
+	Box2DDebugRenderer debugRenderer;
 
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		space = new Texture("sprites/background/blue.png");
+		Box2D.init();
+
+		screenWidth = (int) (Gdx.graphics.getWidth() * SCALE);
+		screenHeight = (int) (Gdx.graphics.getHeight() * SCALE);
 		camera = new OrthographicCamera();
-		shapes = new PhysicsShapeCache("physics.xml");
-		screenWidth = Gdx.graphics.getWidth();
-		screenHeight = Gdx.graphics.getHeight();
 		camera.setToOrtho(false, screenWidth, screenHeight);
+
+		batch = new SpriteBatch();
+		space = new Sprite(new Texture("sprites/background/blue.png"));
+		space.setSize(space.getWidth() * SCALE, space.getHeight() * SCALE);
+
+		world = new World(new Vector2(0,0), true);
+		player = new Ship(world, screenWidth/2, screenHeight/2);
+
+		debugRenderer = new Box2DDebugRenderer();
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.setProjectionMatrix(camera.combined);
-		camera.update();
+
+		world.step(STEP_TIME, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
+		player.controlHandling(getMouseVector());
+
 		batch.begin();
 		//filling background with tiles
-		for(int x = 0; x< screenWidth; x+=space.getWidth())
-			for(int y = 0; y< screenHeight; y+=space.getHeight())
-				batch.draw(space, x, y);
+		for(float x = 0; x< screenWidth; x+=space.getWidth())
+			for(float y = 0; y< screenHeight; y+=space.getHeight()){
+				space.setPosition(x,y);
+				space.draw(batch);
+			}
+		player.draw(batch);
 		batch.end();
+
+		debugRenderer.render(world, camera.combined);
 	}
 	@Override
 	public void resize(int width, int height) {
-		screenWidth = width;
-		screenHeight = height;
+		screenWidth = (int) (width * SCALE);
+		screenHeight = (int) (height * SCALE);
 		camera.setToOrtho(false, screenWidth, screenHeight);
 	}
-
 	@Override
 	public void dispose () {
 		batch.dispose();
-		space.dispose();
+		world.dispose();
+		debugRenderer.dispose();
+	}
+
+	private Vector3 getMouseVector(){
+		Vector3 mouse = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+		camera.unproject(mouse);
+		return mouse;
 	}
 }
---
+++ core/src/com/mygdx/game/Ship.java
@@ -0,0 +1,83 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class Ship extends Entity{
+    private int lifes;
+    private int score;
+    private static final float FORWARD_VELOCITY = 0.7f;
+    private static final float STRAFE_VELOCITY = 0.3f;
+
+    Ship(World world, float x, float y)
+    {
+        lifes = 3;
+        score = 0;
+
+        body = SHAPES.createBody("playerShip", world, SCALE, SCALE);
+        body.setFixedRotation(true);
+        body.setTransform(x, y, 0);
+
+        sprite = new Sprite(new Texture("sprites/playerShip3_orange.png"));
+        sprite.setSize(sprite.getWidth() * SCALE, sprite.getHeight() * SCALE);
+    }
+
+    @Override
+    public void collideWith(Entity target) {
+        //TODO: asteroid collision
+        //TODO: laser collision
+    }
+
+    public void controlHandling(Vector3 mousePos) {
+        //rotating ship to mouse cursor
+        Vector3 pos = new Vector3(body.getPosition().x, body.getPosition().y, 0);
+        pos.sub(mousePos);
+        Vector2 direction = new Vector2(0,0);
+        direction.set(pos.x, pos.y);
+        direction.rotate90(0);
+        body.setTransform(body.getPosition(), direction.angleRad());
+        direction.rotate90(0);
+
+        if(Gdx.input.isKeyPressed(Input.Keys.W)) {  //fly towards cursor
+            direction.setLength(FORWARD_VELOCITY);
+            body.applyLinearImpulse(direction, body.getPosition(), true);
+        }
+        if(Gdx.input.justTouched()) {
+            //TODO: Laser shot
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.A) && !Gdx.input.isKeyPressed(Input.Keys.D)) { //strafe to the left
+            direction.setLength(STRAFE_VELOCITY);
+            direction.rotateDeg(90);
+            body.applyLinearImpulse(direction, body.getPosition(), true);
+        }
+        else if(!Gdx.input.isKeyPressed(Input.Keys.A) && Gdx.input.isKeyPressed(Input.Keys.D)) {//strafe to the right
+            direction.setLength(STRAFE_VELOCITY);
+            direction.rotateDeg(-90);
+            body.applyLinearImpulse(direction, body.getPosition(), true);
+        }
+    }
+
+    public int getLifes() {
+        return lifes;
+    }
+
+    public int getScore() {
+        return score;
+    }
+    //Ship's body origin is in the center, so I need to modify sprite render
+    void draw(SpriteBatch batch) {
+        if(body == null)
+            return;
+        Vector2 pos = body.getPosition();
+        sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
+        sprite.setPosition(pos.x - sprite.getOriginX(), pos.y - sprite.getOriginY());
+        sprite.setRotation((float) Math.toDegrees(body.getAngle()));
+        sprite.draw(batch);
+    }
+}
