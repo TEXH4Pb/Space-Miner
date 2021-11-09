b2e21fd14edfdddc2821982eeb0433ed4a1b229c
TEXH4Pb <9digger9@gmail.com>
2021-11-10 02:45:14 +0300

    Create Entity base class

---
+++ core/src/com/mygdx/game/Entity.java
@@ -0,0 +1,31 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.codeandweb.physicseditor.PhysicsShapeCache;
+
+public abstract class Entity {
+    protected static final PhysicsShapeCache SHAPES = new PhysicsShapeCache("physics.xml");
+    static final float SCALE = 0.02f;
+
+    Sprite sprite = null;
+    Body body = null;
+
+    public abstract void collideWith(Entity target);
+
+    Vector2 getPosition()
+    {
+        return body.getPosition();
+    }
+    void draw(SpriteBatch batch) {
+        if(body == null)
+            return;
+        Vector2 pos = body.getPosition();
+        sprite.setOrigin(0, 0);
+        sprite.setPosition(pos.x, pos.y);
+        sprite.setRotation((float) Math.toDegrees(body.getAngle()));
+        sprite.draw(batch);
+    }
+}
