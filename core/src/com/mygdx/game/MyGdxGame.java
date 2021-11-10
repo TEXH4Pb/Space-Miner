962cc91cb183a9446f4e001522a8c70325c162a3
TEXH4Pb <9digger9@gmail.com>
2021-11-10 15:37:02 +0300

    Borders physics

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -14,10 +14,13 @@ import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+import java.util.ArrayList;
+
 public class MyGdxGame extends ApplicationAdapter {
 	static final float STEP_TIME = 1f / 60f;
 	static final int VELOCITY_ITERATIONS = 6;
 	static final int POSITION_ITERATIONS = 2;
+	static final int ASTEROID_COUNT = 20;
 	static final float SCALE = 0.02f;
 
 	SpriteBatch batch;
@@ -26,6 +29,7 @@ public class MyGdxGame extends ApplicationAdapter {
 	int screenWidth, screenHeight;
 	World world;
 	Ship player;
+	ArrayList<Entity> entities;
 	Box2DDebugRenderer debugRenderer;
 
 	@Override
@@ -43,7 +47,17 @@ public class MyGdxGame extends ApplicationAdapter {
 
 		world = new World(new Vector2(0,0), true);
 		player = new Ship(world, screenWidth/2, screenHeight/2);
-
+		entities = new ArrayList<Entity>();
+		entities.add(player);
+		for(float i = 0, x, y; i < ASTEROID_COUNT; ++i)
+		{
+			do {
+				x = screenWidth * (float)Math.random();
+				y = screenHeight * (float)Math.random();
+			}
+			while (player.getPosition().dst(x,y)<5);
+			entities.add(new Asteroid(world, x, y));
+		}
 		debugRenderer = new Box2DDebugRenderer();
 	}
 
@@ -53,17 +67,21 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.setProjectionMatrix(camera.combined);
 
-		world.step(STEP_TIME, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
-		player.controlHandling(getMouseVector());
+		updateGame();
 
 		batch.begin();
 		//filling background with tiles
-		for(float x = 0; x< screenWidth; x+=space.getWidth())
+		for(float x = 0; x< screenWidth; x+=space.getWidth()){
 			for(float y = 0; y< screenHeight; y+=space.getHeight()){
 				space.setPosition(x,y);
 				space.draw(batch);
 			}
-		player.draw(batch);
+		}
+		//rendering all entities
+		//player.draw(batch);
+		for (Entity e: entities) {
+			e.draw(batch);
+		}
 		batch.end();
 
 		debugRenderer.render(world, camera.combined);
@@ -80,7 +98,38 @@ public class MyGdxGame extends ApplicationAdapter {
 		world.dispose();
 		debugRenderer.dispose();
 	}
+	//all non-render logic
+	private void updateGame()
+	{
+		world.step(STEP_TIME, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
 
+		Vector2 newPos;
+		for (Entity e : entities) {
+			if (e.getPosition().x > screenWidth) {
+				newPos = e.getPosition();
+				newPos.x = 0;
+				e.body.setTransform(newPos, e.body.getAngle());
+			}
+			else if (e.getPosition().x < 0){
+			newPos = e.getPosition();
+			newPos.x = screenWidth;
+			e.body.setTransform(newPos, e.body.getAngle());
+			}
+
+			if(e.getPosition().y > screenHeight) {
+				newPos = e.getPosition();
+				newPos.y = 0;
+				e.body.setTransform(newPos, e.body.getAngle());
+			}
+			else if(e.getPosition().y < 0){
+				newPos = e.getPosition();
+				newPos.y = screenHeight;
+				e.body.setTransform(newPos, e.body.getAngle());
+			}
+		}
+
+		player.controlHandling(getMouseVector());
+	}
 	private Vector3 getMouseVector(){
 		Vector3 mouse = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
 		camera.unproject(mouse);
